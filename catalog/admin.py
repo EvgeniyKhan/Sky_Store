26c9863362be35a3977c9a5e14b6d6f02a806008
evgeniy <evgeniy.igorevich9402@gmail.com>
2024-03-12 10:23:26 +0500

    Для моделей категории и продукта настроил отображение в административной панели

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category_name')
+    list_filter = ('category_name',)
+    search_filter = ('category_name', 'category_description')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product_name', 'category', 'price')
+    list_filter = ('category',)
+    search_fields = ('product_name', 'description')
