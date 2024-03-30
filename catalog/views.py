39fb4703f9aa104c32a5d5f04957fc67e860c030
evgeniy <evgeniy.igorevich9402@gmail.com>
2024-03-30 19:23:58 +0500

    fix

--- catalog/views.py
+++ catalog/views.py
@@ -28,16 +28,21 @@ class ProductListView(ListView):
         return context_data
 
 
-class ProductView(View):
+class ContactsView(View):
     def get(self, request):
-        return render(request, 'catalog:contacts')
+        return render(request, 'catalog/contacts.html')
 
-    def post(self, request):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Контакты'
+        return context
+
+    def post(self, request, *args, **kwargs):
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
         print(f'Name: {name}, Phone: {phone}, Message: {message}')
-        return render(request, 'catalog:contacts')
+        return super().get(request, *args, **kwargs)
 
 
 class ProductDetailView(DetailView):
