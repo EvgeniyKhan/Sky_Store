b26ccc2db36b9b46357e18794e0fad0ea9cb338f
evgeniy <evgeniy.igorevich9402@gmail.com>
2024-04-04 00:39:47 +0500

    добавил модели для Users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='Почта')
+
+    phone = models.CharField(max_length=35, verbose_name='Телефон', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='Аватар', **NULLABLE)
+    country = models.CharField(max_length=70, verbose_name='Страна', **NULLABLE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
