77c13fb02724a01a4bd98b66b11e22849508a8c0
evgeniy <evgeniy.igorevich9402@gmail.com>
2024-04-15 00:22:12 +0500

    добавил урлы для кэша

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,16 +1,18 @@
 from django.urls import path
 
+from django.views.decorators.cache import cache_page
 from catalog.apps import CatalogConfig
 from catalog.views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView, \
-    ContactsView
+    ContactsView, CategoriesListView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', ProductListView.as_view(), name='product_list'),
+    path('', cache_page(60)(ProductListView.as_view()), name='product_list'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
     path('view/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
     path('create/', ProductCreateView.as_view(), name='create_product'),
     path('update/<int:pk>/', ProductUpdateView.as_view(), name='update_product'),
     path('delete/<int:pk>/', ProductDeleteView.as_view(), name='delete_product'),
+    path('categories_list/', cache_page(60)(CategoriesListView.as_view()), name='categories'),
 ]
